8031f18c7aeca2562a679372505776c6ed687638
NexusDevsystem <nexusdevsystem@gmail.com>
2025-05-05 22:59:43 -0300

    Já não mexemos mais no back, mas só pra confirmar o estado

--- src/models/User.js
+++ src/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   name:     { type: String, required: true },
   email:    { type: String, required: true, unique: true },
   password: { type: String, required: true, select: false },
-  role:     { type: String, enum: ['aluno','professor','admin'], default: 'aluno' }
+  role:     { type: String, enum: ['student','professor'], default: 'student' }
 }, { timestamps: true })
 
 userSchema.pre('save', async function(next) {
